b05d47a3b3c005ed68d2f3c6d8cf73ba138a0f7e
hoangan1610 <ngohoangan0@gmail.com>
2025-05-05 09:33:24 +0700

    Your message about the changes

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 // constants/config.js
 
-export const BASE_URL='http://192.168.1.22:3001';
+export const BASE_URL='http://192.168.1.23:3001';
 
 export const CLOUDINARY_UPLOAD_PRESET='garahqa';
 export const CLOUDINARY_URL='https://api.cloudinary.com/v1_1/ddvixbiij/image/upload';
